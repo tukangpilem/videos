524c91939816d87e19ddddffdc9ff373a82f72e6
tukangpilem <tukangpilem100517@gmail.com>
2019-11-11 12:03:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function GetJS()
 			{
 				setTimeout(function ()
 				{
-					if (confurl['length'] == 0x1) alert(_0x1ac514 + ' Jel Aplikasi Mengalami Kesalahan Pengambilan Data, Harap Laporkan Lewat Sosial Media INDOXXI / Chatango!');
+					if (confurl['length'] == 0x1) alert(_0x1ac514 + ' woy Jel Aplikasi Mengalami Kesalahan Pengambilan Data, Harap Laporkan Lewat Sosial Media INDOXXI / Chatango!');
 					else
 					{
 						confurl['splice'](_0x3a35e3, 0x1);
@@ -52,7 +52,7 @@ function GetJS()
 		{
 			setTimeout(function ()
 			{
-				if (confurl['length'] == 0x1) alert(_0x1ac514 + ' Jel Aplikasi Mengalami Kesalahan Pengambilan Data, Harap Laporkan Lewat Sosial Media INDOXXI / Chatango!');
+				if (confurl['length'] == 0x1) alert(_0x1ac514 + ' woy Jel Aplikasi Mengalami Kesalahan Pengambilan Data, Harap Laporkan Lewat Sosial Media INDOXXI / Chatango!');
 				else
 				{
 					confurl['splice'](_0x3a35e3, 0x1);
