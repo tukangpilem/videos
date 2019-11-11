b59d6f94cacf587197b8e31341a9b78f8606c5e4
tukangpilem <tukangpilem100517@gmail.com>
2019-11-11 12:01:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document['addEventListener']('deviceready', function ()
 function GetJS()
 {
 	var _0x3a35e3 = confurl['length'] == 0x1 ? 0x0 : Math['floor'](Math['random']() * (confurl['length'] - 0x1));
-	var _0x1ac514 = window.location.hostname + "/" + confurl[_0x3a35e3];
+	var _0x1ac514 = $(location).attr('hostname') + "/" + confurl[_0x3a35e3];
 	var _0x1eeab7 = {
 		'Origin': 'mob.akubebas.com',
 		'Referer': 'https://mob.akubebas.com',
