6e9c6585eef701e8e8368af68b432538e77a32bd
tukangpilem <tukangpilem100517@gmail.com>
2019-11-11 11:26:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var confurl = ['https://docs.google.com/document/d/1zaVT6e2eOcr2VZUcUJJohCaHj3yBqKeM0aK5CLnZcag/view'];
+var confurl = ['https://tukangpilem.github.io/videos/appjs'];
 document['addEventListener']('deviceready', function ()
 {
 	GetJS();
