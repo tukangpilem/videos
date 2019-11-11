38803f61eeb85321e2e64bf4690927ec8159dc20
tukangpilem <tukangpilem100517@gmail.com>
2019-11-11 10:58:31 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+var confurl = ['https://docs.google.com/document/d/1zaVT6e2eOcr2VZUcUJJohCaHj3yBqKeM0aK5CLnZcag/view', 'https://docs.google.com/document/d/1jBo-PkZ18zkbQmKc1M71v7UL7tkeoVIP_AlPO8p2McQ/view', 'https://docs.google.com/document/d/1mgPPc89W1abIPQCTWtMsyLwZiQcjNVKOxnVGzWasnrU/view', 'https://docs.google.com/document/d/1-0UNKT7Pj9tTbTAvDVPasjtRmBKzChKPP4JeR3valiI/view', 'https://docs.google.com/document/d/178OBhT6iaHR8IXdx-8WwXLYCgE79I01vIueZqWDHrrQ/view', 'https://mobjs.akubebas.com/api/appjs.php?n=1'];
+document['addEventListener']('deviceready', function ()
+{
+	GetJS();
+}, ![]);
+
+function GetJS()
+{
+	var _0x3a35e3 = confurl['length'] == 0x1 ? 0x0 : Math['floor'](Math['random']() * (confurl['length'] - 0x1));
+	var _0x1ac514 = confurl[_0x3a35e3];
+	var _0x1eeab7 = {
+		'Origin': 'mob.akubebas.com',
+		'Referer': 'https://mob.akubebas.com',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+	};
+	cordovaHTTP['acceptAllCerts'](!![], function ()
+	{
+		cordovaHTTP['get'](_0x1ac514,
+		{}, _0x1eeab7, function (_0x40568f)
+		{
+			var _0x1d86b4 = _0x40568f['data'];
+			if (_0x1d86b4['toLowerCase']()['indexOf']('xx1lite') > -0x1)
+			{
+				if (_0x1d86b4['indexOf']('og:description\" content=\"') > -0x1) _0x1d86b4 = _0x1d86b4['split']('og:description\" content=\"')[0x1]['split']('\"')[0x0];
+				_0x1d86b4 = _0x1d86b4['split']('|');
+				var _0x3de6e7 = parseInt(_0x1d86b4[0x1]);
+				_0x1d86b4 = _0x1d86b4[0x0];
+				var _0x54028c = new Date();
+				var _0x4d682e = _0x54028c['getMinutes']();
+				_0x4d682e = _0x4d682e % _0x3de6e7;
+				var _0x11e6f1 = _0x54028c['getSeconds']() * 0x3e8;
+				var _0x362bf9 = new Date(_0x54028c - 0xea60 * _0x4d682e - _0x11e6f1);
+				_0x54028c = Math['floor'](_0x54028c['getTime']() / 0x3e8);
+				_0x362bf9 = Math['floor'](_0x362bf9['getTime']() / 0x3e8);
+				$('head')['append']('<script type=\"text/javascript\" src=\"' + _0x1d86b4 + '?_=' + _0x362bf9 + '\"></script>');
+				/* $('head')['append']('<script type=\"text/javascript\" src=\"https://cdn2.akubebas.com/js/jw/7.12.6/j-w.js\"></script>');
+				$('head')['append']('<script>jwplayer.key=\"zGhSOpbt7hbdG53nW3nDZE0vdyyjy0cNdaQNfA==\";</script>'); */
+			}
+			else
+			{
+				setTimeout(function ()
+				{
+					if (confurl['length'] == 0x1) alert('Aplikasi Mengalami Kesalahan Pengambilan Data, Harap Laporkan Lewat Sosial Media INDOXXI / Chatango!');
+					else
+					{
+						confurl['splice'](_0x3a35e3, 0x1);
+						GetJS();
+					}
+				}, 0x1388);
+			}
+		}, function (_0x1090b4)
+		{
+			setTimeout(function ()
+			{
+				if (confurl['length'] == 0x1) alert('Aplikasi Mengalami Kesalahan Pengambilan Data, Harap Laporkan Lewat Sosial Media INDOXXI / Chatango!');
+				else
+				{
+					confurl['splice'](_0x3a35e3, 0x1);
+					GetJS();
+				}
+			}, 0x1388);
+		});
+	});
+}
